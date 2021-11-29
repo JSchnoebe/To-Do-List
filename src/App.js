d1be19253f2540edf9cf862efb7880a74f5c20ac
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-11-29 16:13:35 -0600

    made ToDos page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./Components/Home"
+import ToDos from './Components/ToDos';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <ToDos />
     </div>
   );
 }
---
+++ src/Components/ToDos.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ToDosClassComponent extends React.Component{
+  render(){
+    const { toDos } = this.props;
+    return(
+      <ul>
+        {toDos.map(toDo =>(
+          <li key={toDo.id}>{toDo.name}</li>
+        ))}
+
+      </ul>
+    )
+  }
+}
+
+export default ToDosClassComponent;
