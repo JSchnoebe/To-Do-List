b93215cfb381ad8941b264aeafb49538309a6c40
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-12-01 22:42:05 -0600

    added some of the Toast

---
+++ src/Components/ToDoItem.js
@@ -0,0 +1,17 @@
+import { Toast } from "react-bootstrap";
+
+// function ToDoItem(props) {
+
+//   const { toDos } = props;
+
+//   return (
+//     <>
+//     {toDos.map(toDo =>(
+//       <Toast onClose={handleDelete} className="mt-4" style={{ width: '32rem' }}  key={task.title}></Toast>
+//     )
+//     )}
+//     </>
+//   )
+// }
+
+// export default ToDoItem;
